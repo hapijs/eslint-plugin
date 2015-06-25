7eff594b46d9bb5150ab5cae6616bbe870bc3c72
Wyatt Preul <wpreul@gmail.com>
2015-06-25 09:37:27 -0500

    Add no-shadow-relaxed rule to plugin

--- test/index.js
+++ test/index.js
@@ -17,9 +17,10 @@ describe('ESLint Plugin', function() {
 
     var rules = Object.keys(Plugin.rules);
 
-    expect(rules.length).to.equal(2);
+    expect(rules.length).to.equal(3);
     expect(rules.indexOf('hapi-capitalize-modules')).to.not.equal(-1);
     expect(rules.indexOf('hapi-scope-start')).to.not.equal(-1);
+    expect(rules.indexOf('no-shadow-relaxed')).to.not.equal(-1);
 
     for (var i = 0; i < rules.length; ++i) {
       expect(Plugin.rules[rules[i]]).to.be.a.function();
